e05c7cfb620266c1b698e5e3e78c11a8029525f4
james7132 <contact@jamessliu.com>
2020-08-08 12:28:45 -0700

    Shrink PlayerComponent to fit into a cache line

--- Assets/Code/src/Match/Components/PlayerComponent.cs
+++ Assets/Code/src/Match/Components/PlayerComponent.cs
@@ -1,31 +1,61 @@
+using System;
+using UnityEngine;
 using Unity.Entities;
 
 namespace HouraiTeahouse.FantasyCrescendo {
 
+[Flags]
+public enum PlayerFlags : byte {
+  FACING_LEFT   = 1 << 0,
+  GROUNDED      = 1 << 1,
+  FAST_FALLING  = 1 << 2,
+  TEETERING     = 1 << 3,
+}
+
 public struct PlayerComponent : IComponentData {
+
+  // 12 bytes
+  public PlayerFlags Flags;                   // 1 byte
+  public float Damage;                        // 4 bytes
+  [SerializeField] byte _stateID;             // 1 bytes
+  [SerializeField] byte _stateTick;           // 1 byte
+  [SerializeField] sbyte _stocks;             // 1 byte
+  [SerializeField] byte _hitstun;             // 1 byte
+  [SerializeField] byte _hitlag;              // 1 byte
+  [SerializeField] byte _charge;              // 1 byte
+
+  public int Stocks {
+    get => (int)_stocks;
+    set => _stocks = (sbyte)value;
+  }
+  public int Hitstun {
+    get => (int)_hitstun;
+    set => _hitstun = (byte)value;
+  }
+  public int Hitlag {
+    get => (int)_hitlag;
+    set => _hitlag = (byte)value;
+  }
+
+  public int StateID {
+    get => (int)_stateID;
+    set => _stateID = (byte)value;
+  }
+  public int StateTick {
+    get => (int)_stateTick;
+    set => _stateTick = (byte)value;
+  }
+
   public bool IsActive => Stocks > 0;
   public bool IsRespawning => RespawnTimeRemaining > 0;
   public bool IsHit => Hitstun > 0;
 
-  // TODO(james7132): Change this into a bit field
-  public bool Direction;
-  public bool IsFastFalling;
-  public bool IsTeetering;
-
-  public int Stocks;
-  public float Damage;
-  public int Hitstun;
-  public int Hitlag;
-
-  public uint StateID;
-  public uint StateTick;
-
-  public byte Charge;
+  public bool Is(PlayerFlags flagMask) => (Flags & flagMask) != 0;
 
   public ushort ShieldDamage;
   public ushort ShieldRecoveryCooldown;
 
-  public uint RespawnTimeRemaining;
+  public ushort RespawnTimeRemaining;
 }
 
 }
